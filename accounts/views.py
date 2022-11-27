8b2d032018614b3a0064f188365aa5486042826c
Yiannis <jtahos@yahoo.com>
2022-11-27 13:13:46 -0500

    register user and funcionalatys

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def registerUser(request):
-    return HttpResponse('This is a user registration form')
+    if request.method == 'POST':
+    
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # create the user using the form
+        
+            # user = form.save(commit=False)
+            # password = form.cleaned_data['password']
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # form.save()
+            
+            # create the user using the createUser method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, 'your account has been created!')
+            
+            return redirect('registerUser')
+            
+        else:
+            print('invalid form')
+            print('Form.errors')
+    else:    
+         form = UserForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
