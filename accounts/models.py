df63b12a2f3686e1202ef3da381cdd5bc9ed9585
Yiannis <jtahos@yahoo.com>
2022-11-29 20:27:39 -0500

    customer and vendor registration complerte

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address')
 
         if not username:
-            raise ValueError('User must have an username')
+            raise ValueError('User must have a username')
 
         user = self.model(
             email = self.normalize_email(email),
@@ -77,6 +77,13 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+    
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
 
 
 class UserProfile(models.Model):
